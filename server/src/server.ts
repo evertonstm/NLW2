9c1a0b6f9c5fd450d06535c96f6356d85ba00b85
Everton Reis <evertonreis@Mac-mini-de-Everton.local>
2020-08-17 23:37:06 -0400

    conexão com DB e  migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,24 +4,8 @@ const app = express();
 
 app.use(express.json());
 
-//GET: Buscar ou lista uma informação
-//POST: Criar alguma nova informação
-//PUT: Atualizar uma informação existente
-//DELETE: Deletar uma informação existente
-
-//Parametros
-//Corpo (request Body): Dados para criação de um registro
-//route params: identificar qual recurso eu quero atualizar ou deletar
-//query Params: paginação, filtros, ordenação
-
-app.post('/users', (request, response ) => {
-  console.log(request.body)
-
-  const users = [
-    {name: "Everton Reis", age:25},
-    {name:"DAvid", age:21},
-  ]
-  return response.json(users)
+app.get('/', (request, response ) => {
+   return response.json({message:'Hello World'})
 });
 
 app.listen(3333);
